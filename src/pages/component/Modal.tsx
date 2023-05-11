d56049089063ae0ece6c1aac816f2566e790851a
huangweilin <896466205@qq.com>
2023-05-11 10:09:15 +0000

    add modal

--- src/pages/component/Modal.tsx
+++ src/pages/component/Modal.tsx
@@ -1,9 +1,18 @@
 import Layout from "@/components/Layout"
+import ExampleModal from "@/components/modal"
+import { useState } from "react"
 
 export default ()=>{
+    const [open,setOpen] = useState(false);
+
     return(
         <Layout>
-            <div>Modal</div>
+            <button className="border px-3 py-1" onClick={()=>setOpen(true)}>Modal</button>
+            <ExampleModal open={open} setOpen={setOpen}>
+                <div>
+                    hello
+                </div>
+                </ExampleModal>
         </Layout>
     )
 }
